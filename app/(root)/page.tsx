f8ebf86db09e1c9ee8874dc78133a3b4a88c1dbf
João Meneses <meneses-joao@hotmail.com>
2024-08-20 20:38:59 -0300

    feat: Add MyBanks, Transfer, and TransactionHistory pages
    This commit adds three new pages: MyBanks, Transfer, and TransactionHistory. Each page is a functional component that renders a simple placeholder text. These pages will serve as the foundation for future development and integration with backend APIs.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import HeaderBox from '@/components/HeaderBox';
+import RigthSideBar from '@/components/RigthSideBar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
+
+    const loggedIn = {firstName: 'João', lastName: 'Meneses', email: 'meneses-joao@hotmail.com '};
+
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    /> 
+                </header>
+
+                RECENT TRANSACTIONS
+            </div>
+
+            <RigthSideBar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance:153.50}, {currentBalance: 1250.35}]}
+            />
+        </section>
     );
 };
 
