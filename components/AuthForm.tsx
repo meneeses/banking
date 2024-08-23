401d0722e0cc01555034c7a2ae3783bca5b3cdd7
João Meneses <meneses-joao@hotmail.com>
2024-08-23 00:20:58 -0300

    feat: Add PlaidLink component to AuthForm
    This commit adds the PlaidLink component to the AuthForm component in order to provide users with the ability to connect their bank account. The PlaidLink component is rendered conditionally based on the user's authentication status.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 const AuthForm = ({ type }: {type: string}) => {
     const router = useRouter()
@@ -38,8 +39,22 @@ const AuthForm = ({ type }: {type: string}) => {
             setIsLoading(true)
             try {
                 //Sign-in with Appwrite & create plain link token 
+
                 if (type === 'sign-up') {
-                   const newUser = await signUp(data)
+                    const userData = {
+                        firstName: data.firstName!,
+                        lastName: data.lastName!,
+                        address1: data.address1!,
+                        city: data.city!,
+                        state: data.state!,
+                        postalCode: data.postalCode!,
+                        dateOfBirth: data.dateOfBirth!,
+                        ssn: data.ssn!,
+                        email: data.email!,
+                        password: data.password!
+                    }
+
+                   const newUser = await signUp(userData)
                     setUser(newUser)
                 } 
 
@@ -90,11 +105,14 @@ const AuthForm = ({ type }: {type: string}) => {
             </h1>
         </div>
         </header>
-        {user ? (
+        {/* {user ? ( */}
             <div className='flex flex-col gap-4'>
-                {/* PlaidLink */}
+                <PlaidLink 
+                user={user}
+                variant='primary'
+                />
             </div>
-        ): (
+        {/* ): ( */}
             <>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -180,7 +198,7 @@ const AuthForm = ({ type }: {type: string}) => {
                 </Link>
             </footer>
             </>
-        )}
+        {/* )} */}
     </section>
   )
 }
